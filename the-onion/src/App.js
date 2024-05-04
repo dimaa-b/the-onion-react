fd9391a4650190c115de247673c51d9f0e6cd261
dimaa-b <70352157+dimaa-b@users.noreply.github.com>
2024-05-04 10:43:16 +0000

    initial commit

---
+++ the-onion/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  /*
+    On load make a call to the Gemini API, and put the content onto the screen as it's being recieved
+  */
+  useEffect(() => {
+
+  })
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ the-onion/src/components/ArticleView.js
@@ -0,0 +1,3 @@
+export default async function ArticleView({title, tags, description}) {
+    // The article will be generated on the fly. 
+}
